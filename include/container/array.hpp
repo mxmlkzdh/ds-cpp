26e650307d11b4016f05c54e8c64cf8f5619b520
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-03-22 21:59:34 -0400

    Implement the first version of Max::Array

--- include/container/array.hpp
+++ include/container/array.hpp
@@ -4,8 +4,78 @@
 
 namespace Max {
 
-template <typename T, size_t N>
+template <typename _T, std::size_t _Size>
 class Array {
+
+    static_assert(_Size > 0, "Size of Max::Array should be positive.");
+
+    using SizeType = std::size_t;
+    using ValueType = _T;
+    using Reference = ValueType&;
+    using ConstReference = const _T&;
+    using Pointer = ValueType*;
+    using ConstPointer = const _T*;
+
+private:
+    ValueType mData[_Size];
+
+public:
+
+    // Special Members
+    Array() = default;
+    ~Array() = default;
+    Array(const Array& other) = default;
+    Array(Array&& other) = default;
+    Array& operator=(const Array& other) = default;
+    Array& operator=(Array&& other) = default;
+
+    void fill(ConstReference data) {
+        for (SizeType i = 0; i < _Size; i++) {
+            mData[i] = data;
+        }
+    }
+
+    // Element Access
+    Reference operator[](const SizeType index) {
+        return mData[index];
+    }
+    ConstReference operator[](const SizeType index) const {
+        return mData[index];
+    }
+    Reference at(const SizeType index) {
+        if (index >= _Size) {
+            throw("Index out of bound!");
+        }
+        return mData[index];
+    }
+    ConstReference at(const SizeType index) const {
+        if (index >= _Size) {
+            throw("Index out of bound!");
+        }
+        return mData[index];
+    }
+    Reference front() {
+        return mData[0];
+    }
+    ConstReference front() const {
+        return mData[0];
+    }
+    Reference back() {
+        return mData[_Size - 1];
+    }
+    ConstReference back() const {
+        return mData[_Size - 1];
+    }
+    Pointer data() noexcept {
+        return mData;
+    }
+    ConstPointer data() const noexcept {
+        return mData;
+    }
+
+    SizeType size() const {
+        return _Size;
+    }
 
 };
 
--- tests/container/array.test.cpp
+++ tests/container/array.test.cpp
@@ -1,2 +1,35 @@
 #include "container/array.hpp"
 #include <gtest/gtest.h>
+#include <string>
+
+TEST(MaxArray, ArraySize) {
+    Max::Array<int, 17> tempArray;
+    EXPECT_EQ(tempArray.size(), 17);
+}
+
+TEST(MaxArray, SizeOfArray) {
+    Max::Array<std::string, 23> tempArray;
+    EXPECT_EQ(sizeof(tempArray), sizeof(std::string) * tempArray.size());
+}
+
+TEST(MaxArray, Front) {
+    Max::Array<int, 3> tempArray;
+    tempArray[0] = 11;
+    tempArray[1] = 15;
+    tempArray[2] = 19;
+    EXPECT_EQ(tempArray.front(), 11);
+}
+
+TEST(MaxArray, Back) {
+    Max::Array<int, 3> tempArray;
+    tempArray[0] = 11;
+    tempArray[1] = 15;
+    tempArray[2] = 19;
+    EXPECT_EQ(tempArray.back(), 19);
+}
+
+TEST(MaxArray, Data) {
+    Max::Array<int, 5> tempArray;
+    tempArray[3] = 15;
+    EXPECT_EQ(*(tempArray.data() + 3), 15);
+}
