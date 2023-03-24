d41acb8bde09e4e2ae1aabb707f097d4cff35669
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-03-24 09:33:44 -0400

    Add basic Max::Vector implementation

--- include/container/vector.hpp
+++ include/container/vector.hpp
@@ -1,11 +1,111 @@
 #pragma once
 
 #include <cstddef>
+#include <spdlog/spdlog.h>
 
 namespace Max {
 
-template <typename T>
+template <typename _T>
 class Vector {
+
+    using SizeType = std::size_t;
+    using ValueType = _T;
+    using Reference = ValueType&;
+    using ConstReference = const _T&;
+    using Pointer = ValueType*;
+    using ConstPointer = const _T*;
+
+private:
+
+    ValueType* mData;
+    SizeType mSize;
+    SizeType mCapacity;
+
+    void reAlloc(SizeType newSize) {
+        spdlog::debug("reAlloc with the new size {}.", newSize);
+        ValueType* temp = new ValueType[newSize];
+        for (size_t i = 0; i < mSize; i++) {
+            temp[i] = mData[i];
+        }
+        delete[] mData;
+        mData = temp;
+        mCapacity = newSize;
+    }
+
+public:
+    // Special Members
+    Vector(): mData(nullptr), mSize(0), mCapacity(0) {
+        spdlog::debug("Vector creations starts.");
+        reAlloc(1);
+        spdlog::debug("Vector created.");
+    }
+    ~Vector() {
+        spdlog::debug("Vector destruction starts.");
+        delete[] mData;
+        spdlog::debug("Vector destroyed.");
+    }
+    Vector(const Vector& other) = default;
+    Vector(Vector&& other) = default;
+    Vector& operator=(const Vector& other) = default;
+    Vector& operator=(Vector&& other) = default;
+
+   // Element Access
+    Reference operator[](const SizeType index) {
+        return mData[index];
+    }
+    ConstReference operator[](const SizeType index) const {
+        return mData[index];
+    }
+    Reference front() {
+        if (empty()) {
+            throw("Empty array!");
+        }
+        return mData[0];
+    }
+    ConstReference front() const {
+        if (empty()) {
+            throw("Empty array!");
+        }
+        return mData[0];
+    }
+    Reference back() {
+        if (empty()) {
+            throw("Empty array!");
+        }
+        return mData[mSize - 1];
+    }
+    ConstReference back() const {
+        if (empty()) {
+            throw("Empty array!");
+        }
+        return mData[mSize - 1];
+    }
+
+    // Element Insertion
+    void push_back(ValueType value) {
+        if (mSize == mCapacity) {
+            reAlloc(mSize * 2);
+        }
+        mData[mSize++] = value;
+    }
+
+    void emplace_back(Reference value) {
+        if (mSize == mCapacity) {
+            reAlloc(mSize * 2);
+        }
+    }
+
+    /// @brief Get the size of the vector.
+    /// @return The size of the vector.
+    SizeType size() const {
+        return mSize;
+    }
+
+    /// @brief Indicates if the vector is empty.
+    /// @return true if vector is empty and false otherwise.
+    bool empty() const {
+        return mSize == 0;
+    }
 
 };
 
--- tests/container/vector.test.cpp
+++ tests/container/vector.test.cpp
@@ -1,2 +1,39 @@
 #include "container/vector.hpp"
 #include <gtest/gtest.h>
+#include <string>
+
+TEST(MaxVector, VectorSize) {
+    Max::Vector<int> tempVector;
+    tempVector.push_back(1);
+    tempVector.push_back(2);
+    tempVector.push_back(3);
+    tempVector.push_back(4);
+    tempVector.push_back(5);
+    EXPECT_EQ(tempVector.size(), 5);
+}
+
+TEST(MaxVector, Front) {
+    Max::Vector<int> tempVector;
+    tempVector.push_back(1);
+    tempVector.push_back(2);
+    tempVector.push_back(3);
+    EXPECT_EQ(tempVector.front(), 1);
+}
+
+TEST(MaxVector, Back) {
+    Max::Vector<int> tempVector;
+    tempVector.push_back(1);
+    tempVector.push_back(2);
+    tempVector.push_back(3);
+    EXPECT_EQ(tempVector.back(), 3);
+}
+
+TEST(MaxVector, EmptyVectorFront) {
+    Max::Vector<int> tempVector;
+    EXPECT_THROW(tempVector.front(), const char*);
+}
+
+TEST(MaxVector, EmptyVectorBack) {
+    Max::Vector<int> tempVector;
+    EXPECT_THROW(tempVector.back(), const char*);
+}
