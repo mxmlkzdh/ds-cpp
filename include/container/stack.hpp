3bc86599979c4bd5f207d23e38a80729d438879e
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-03-29 21:08:25 -0400

    Add typealiases

--- include/container/stack.hpp
+++ include/container/stack.hpp
@@ -7,6 +7,14 @@ namespace Max {
 template <typename _T>
 class Stack {
 
+    using SizeType = std::size_t;
+    using ValueType = _T;
+    using Reference = ValueType&;
+    using ConstReference = const _T&;
+    using Pointer = ValueType*;
+    using ConstPointer = const _T*;
+    using TemporaryReference = ValueType&&;
+    
 };
 
 }
